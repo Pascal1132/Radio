6eb924533561ad0f64c9877d9d64df7c3392f501
Pascal Parent <p.parent@cameleonmedia.com>
2021-02-02 14:37:59 -0500

    wip

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo rtl_fm -M wbfm -f 94.3M -r 48000 -p 48 -g 49.6 | lame -r -s 48.0 -m m - - | ezstream -c /home/pi/ezstream/testp.xml
+sudo rtl_fm -M wbfm -f $1M -r 48000 -p 48 -g 49.6 | lame -r -s 48.0 -m m - - | ezstream -c /home/pi/ezstream/testp.xml
+
